4883e10d8e508dbaceda312d7797f067cc2da2c0
Neolnevergiveup <1203237924@qq.com>
2019-04-03 23:56:06 +0800

    登录做完了

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 function login(){
     var res = check();
     if (res) {
-        console.log('login')
         $.post(URL + "/buy/index/login", {
             code: $('.account').val(),
-            pwd: $('.pwd').val()
+            pwd: $('.pwd').val(),
+            open_id: localStorage.open_id
         }, function(res, status){
             if ('success' == status) {
                 if (res.code === 0) {
@@ -12,11 +12,11 @@ function login(){
                         content:'成功!',
                         duration:2000,
                     });
-                    setTimeout(function(){
-                        window.location.href = './login.html';
-                    }, 2000)
                 } else {
-                    alert(res.msg)
+                    $('body').toast({
+                        content:res.msg,
+                        duration:2000,
+                    });
                 }
             }
         });
@@ -25,20 +25,56 @@ function login(){
 // 检查输入
 function check(){
     if (!$('.account').val().trim()) {
-        alert('请输入企业账号')
+        $('body').toast({
+            content:'请输入企业账号',
+            duration:2000,
+        });
         return false
     }
     if (!$('.pwd').val().trim()) {
-        alert('请输入密码')
+        $('body').toast({
+            content:'请输入密码',
+            duration:2000,
+        });
         return false
     }
     return true
+}
+// 获得参数code
+function getUrlParam(name){
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2])
+    return null
+}
+// 由code获得openid
+function getOpenId(code){
+    $.get(URL + "/buy/index/getOpenId?code=" + code,function(res,status){
+        if ('success' == status) {
+            if (res.code === 0) {
+                localStorage.setItem('open_id', res.data.open_id)
+            } else {
+                $('body').toast({
+                    content:res.msg,
+                    duration:2000,
+                });
+            }
+        } else {
+            $('body').toast({
+                content:'请求行业列表失败',
+                duration:2000,
+            });
+        }
+    });
 }
 $(function(){
     // 获取code
     const appid = 'wx679a63f8ef2a7591';
     const url = window.location.href;
-    // const url = '116.24.66.100';
-    console.log(url)
-    // window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+encodeURIComponent(url)+'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+    const code = getUrlParam('code');
+    if (code == null || code === '') {
+        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+encodeURIComponent(url)+'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+    } else {
+        getOpenId(code)
+    }
 })

48db7d85cc013d8f319bd7ef649e1c841b0b21a3
Neolnevergiveup <1203237924@qq.com>
2019-04-13 17:45:56 +0800

    微信支付做完待测

--- js/buy.js
+++ js/buy.js
@@ -1,8 +1,10 @@
+var price = ''
 // 加一
 function plus() {
     var num = parseInt($('#amount').val());
     num++;
     $('#amount').attr('value', num)
+    $('#money').html(price * num)
 }
 // 减一
 function reduce() {
@@ -10,9 +12,53 @@ function reduce() {
     num--;
     if (num >= 0) {
         $('#amount').attr('value', num)
+        $('#money').html(price * num)
     }
 }
 // 前往支付
 function goPay(){
-    window.location.href = './pay.html'
+    var num = parseInt($('#amount').val());
+    window.location.href = './pay.html?num='+num+'&price='+price;
 }
+// 获取url参数
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+$(function(){
+    $.get(URL + "/buy/index/comfirmOrder?open_id="+localStorage.open_id, function(res,status){
+        if ('success' == status) {
+            if (res.code === 0) {
+                console.log(res)
+                $('#priceTitle').html(res.data.banner.tips)
+                $('#price').html(res.data.banner.price)
+                $('#unit').html(res.data.banner.spec)
+                $('#redInfo').html(res.data.banner.tips)
+                var infoList = res.data.bottom_tips.spec
+                var str = ''
+                for(var i = 0; i < infoList.length; i++) {
+                    str += "<p>" + infoList[i] + "</p>"
+                }
+                $('#content').html(str)
+                price = parseFloat(res.data.banner.price)
+                var num = getQueryString('num');
+                if (num) {
+                    console.log('有num');
+                    $('#amount').attr('value', num);
+                    $('#money').html(num * price);
+                }
+            } else {
+                $('body').toast({
+                    content:'请求失败',
+                    duration:2000,
+                });
+            }
+        } else {
+            console.log('获取信息失败');
+        }
+    });
+})

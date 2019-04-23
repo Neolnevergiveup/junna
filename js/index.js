65400fa56a24c3e36e85416f9fc4e43bd270441d
Neolnevergiveup <1203237924@qq.com>
2019-04-23 19:28:45 +0800

    返回检查修改

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 $(function(){
     getData()
 })
-// 返回登录页
-function back () {
-    localStorage.removeItem('open_id')
-    window.location.href = './login.html'
-}
 // 获得页面数据
 function getData(){
     $.get(URL + "/buy/index/index?open_id="+localStorage.open_id, function(res,status){
